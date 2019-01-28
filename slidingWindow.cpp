72906ad2ccfe94e3207db3859b52ce4f4710916d
blackwut <albertoottimo@gmail.com>
2019-01-28 11:15:48 +0100

    Added Sliding Window solution

---
+++ slidingWindow.cpp
@@ -0,0 +1,89 @@
+/**
+    Author:  Alberto Ottimo
+    Problem: https://practice.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k/0
+
+    Solution Description
+    This solution make use of a double ended queue that stores indices of the
+    elements inside the current window, "sorted" by element value.
+    Each time an index is inserted in the back of the queue, all indices 
+    refering to elements smaller than the one pointed by the index, are removed
+    from back.
+    Each time the window moves, all the indices outside the window are removed
+    from front.
+    In this way all the elements in the queue are only the right-leader of the
+    current window.
+    For each window, the element pointed by the front index in the queue is
+    stored in a vector.
+
+    Time  Complexity: O(N)
+    Space Complexity: O(N)
+*/
+
+#include <iostream>
+#include <vector>
+#include <deque>
+using namespace std;
+
+vector<int> maxsSubarray(const vector<int> & v, int K)
+{
+    vector<int> result;
+    result.reserve(v.size() - K + 1);
+    std::deque<int> d;
+
+    for (int i = 0; i < v.size(); ++i) {
+
+        // Removes elements no longer in the window
+        while (!d.empty() and d.front() <= i - K) {
+            d.pop_front();
+        }
+
+        // Removes elements smaller than the current element
+        while (!d.empty() and v[d.back()] <= v[i]) {
+            d.pop_back();
+        }
+
+        // Add the current element
+        d.push_back(i);
+
+        // Store the max element in the window into result
+        if (i >= K - 1) {
+            result.push_back(v[d.front()]);
+        }
+    }
+
+    return result;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+    cin >> T;
+
+    for (int t = 0; t < T; ++t) {
+        int N;
+        int K;
+        cin >> N;
+        cin >> K;
+        vector<int> v;
+        v.reserve(N);
+        for (int n = 0; n < N; ++n) {
+            int x;
+            cin >> x;
+            v.push_back(x);
+        }
+
+        vector<int> result = maxsSubarray(v, K);
+        for (auto i = result.begin(); i != result.end(); ++i) {
+            cout << *i << " ";
+        }
+        cout << endl;
+
+        result.clear();
+        v.clear();
+    }
+
+    return 0;
+}
