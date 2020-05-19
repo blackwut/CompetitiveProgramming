80abec1db3f10fd5cc07875d03c06c4208c0e242
blackwut <albertoottimo@gmail.com>
2020-05-19 16:58:27 +0200

    Completed

--- 07_findingTeamMember.cpp
+++ 07_findingTeamMember.cpp
@@ -3,24 +3,31 @@
     Problem: http://codeforces.com/problemset/problem/579/B?locale=en
 
     Solution Description
-    TODO: complete
+    A team is a tuple (i, j, s), formed by the teammate i, the teammate j and
+    the strength s, which is the team strength considering these teammates.
+    Sort by strength all the teams in decreasing order. Then iterate the sorted
+    teams, assigning at each teammate the other teammate's id, only if both of
+    them are not yet in a team.
 
-    Time  Complexity: O()
-    Space Complexity: O()
+    Time  Complexity: O(N^2)
+    Space Complexity: O(N^2)
+
+    Where N is the number of teams to find.
 */
 
 #include <iostream>
 #include <algorithm>
 #include <vector>
+
 using namespace std;
 
 struct Team
 {
-    size_t i;
-    size_t j;
-    int32_t s;
+    int i;
+    int j;
+    int s;
 
-    Team(size_t i, size_t j, size_t s) :
+    Team(int i, int j, int s) :
     i(i),
     j(j),
     s(s)
@@ -32,37 +39,38 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    size_t N;
+    int N;
     cin >> N;
     vector<Team> teams;
     teams.reserve( N * (N - 1) / 2 );
 
-    for (size_t i = 0; i < (2 * N); ++i) {
-        for (size_t j = 0; j < i; ++j) {
-            int32_t s;
+    for (int i = 0; i < (2 * N); ++i) {
+        for (int j = 0; j < i; ++j) {
+            int s;
             cin >> s;
             teams.emplace_back(i, j, s);
         }
     }
 
     sort(teams.begin(), teams.end(),
-        [](const Team & l, const Team & r) { return l.s > r.s; });
+        [](const Team & lhs, const Team & rhs) { return lhs.s > rhs.s; });
 
-    vector<size_t> teammate( 2 * N );
+    vector<int> teammates( 2 * N );
 
-    for(Team & t: teams) {
-        if(teammate[t.i] == 0 and teammate[t.j] == 0) {
-            teammate[t.i] = t.j + 1;
-            teammate[t.j] = t.i + 1; 
+    for(const Team & t: teams) {
+        // if both teammates are not in a team
+        if(teammates[t.i] == 0 and teammates[t.j] == 0) {
+            teammates[t.i] = t.j + 1; // store the id of its teammate j
+            teammates[t.j] = t.i + 1; // store the id of its teammate i
         }
     }
 
-    for (auto i = teammate.begin(); i < teammate.end(); ++i ) {
-        cout << *i << " ";
+    for (const auto & i : teammates ) {
+        cout << i << " ";
     }
     cout << endl;
 
-    teammate.clear();
+    teammates.clear();
     teams.clear();
 
     return 0;
