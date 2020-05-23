f3a8effe46c665415f2761f2bbbfaf55f48bd745
blackwut <albertoottimo@gmail.com>
2020-05-23 17:16:23 +0200

    Completed

--- 24_nestedSegments_ST.cpp
+++ 24_nestedSegments_ST.cpp
@@ -1,143 +1,160 @@
 /**
     Author:  Alberto Ottimo
-    Problem: 
+    Problem: https://codeforces.com/problemset/problem/652/D?locale=en
 
     Solution Description
-    
+    Sort the segments <l_i, r_i> by r_i and calculate the rank of each segment.
+    Sort segments <l_i, r_i> in decreasing order by l_i.
+    Make use of a ST (Segment Tree to keep) track, using the rank as index, of
+    the already visited segments. For each segment processed, add 1 to
+    the ST at its rank, then get the sum from 0 to its r_i, that is the number
+    of nested segments.
 
-    Time  Complexity: O()
-    Space Complexity: O()
+    Time  Complexity: O(N log N)
+    Space Complexity: O(N)
 */
 
 #include <iostream>
+#include <algorithm>
 #include <vector>
-#include <limits>
-#include "Structures/SegmentTree.hpp"
+#include <tuple>
+
 using namespace std;
 
+#define SUBMIT_ONLINE 1
+#if !SUBMIT_ONLINE
+#include "Structures/SegmentTree.hpp"
+#else
+#include <functional>
 
 template<typename T>
-inline void do_not_optimize_away(T&& datum) {
-    asm volatile("" : "+r" (datum));
-}
+struct SegmentTree
+{
+private:
+    int n;
+    vector<T> tree;
+    function<T(const T &, const T &)> op;
+    T id;
 
-template<typename ValueType>
-struct min_operator {
-    ValueType operator()(ValueType x, ValueType y) {
-        return std::min<ValueType>(x, y);
+public:
+
+    SegmentTree(function<T(const T &, const T &)> binaryOperation,
+                const T identityValue)
+    : op(binaryOperation),
+      id(identityValue)
+    {}
+
+    void buildConstant(const int size, const T & val)
+    {
+        n = size;
+        tree.resize(2 * n, id);
+        for(int i = 0; i < n; ++i) {
+            tree[n + 1] = val;
+        }
+
+        for (int i = n - 1; i > 0; --i) {
+            tree[i] = op(tree[i << 1], tree[i << 1 | 1]);
+        }
+    }
+
+    void build(const vector<T> & v)
+    {
+        n = v.size();
+        tree.resize(2 * n, id);
+        for(int i = 0; i < n; ++i) {
+            tree[n + 1] = v[i];
+        }
+
+        for (int i = n - 1; i > 0; --i) {
+            tree[i] = op(tree[i << 1], tree[i << 1 | 1]);
+        }
+    }
+
+    void update(int pos, T delta)
+    {
+        tree[n + pos] += delta;
+
+        for (int i = pos + n; i > 1; i >>= 1) {
+            tree[i >> 1] = op(tree[i], tree[i ^ 1]);
+        }
+    }
+
+    T query(int l, int r)
+    {
+        T result = id;
+
+        l += n;
+        r += n;
+        for (; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) result = op(result, tree[l++]);
+            if (r & 1) result = op(result, tree[--r]);
+        }
+        return result;
     }
 };
+#endif
+
+
+#define RANGE tuple<int, int, int>
+#define R_LEFT    0
+#define R_RIGHT   1
+#define R_RANK    2
 
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    uint64_t n = 0; // number of leaves in the tree
-    std::cin >> n;
+    int N;
+    cin >> N;
 
-    typedef std::chrono::high_resolution_clock clock_type;
+    // <left, right, rank>
+    vector< RANGE > v(N);
+    vector<int> results(N);
 
-    auto start = clock_type::now();
-    std::vector<int> leaves;
-    leaves.reserve(n);
-    for (uint64_t i = 0; i < n; ++i) {
-        int x = 0;
-        std::cin >> x;
-        leaves.push_back(x);
-    }
-    auto end = clock_type::now();
-    std::chrono::duration<double> elapsed = end - start;
-    std::cout << "parsing the input took: " << elapsed.count() << " [sec]" << std::endl;
-    typedef SegmentTree<int, min_operator<int>> segment_tree_type;
-
-
-    int max = numeric_limits<int>::max();
-
-    std::cout << "building tree with " << n << " leaves" << std::endl;
-    start = clock_type::now();
-    SegmentTree<int, min_operator<int>> min_seg_tree(min_operator<int>(), max);
-    min_seg_tree.build(leaves);
-    end = clock_type::now();
-    elapsed = end - start;
-    std::cout << "building took: " << elapsed.count() << " [sec]" << std::endl;
-
-    uint64_t m = 0; // number of queries
-    std::cin >> m;
-
-    std::vector<segment_tree_type::STRange> queries;
-    queries.reserve(m);
-
-    for (uint64_t i = 0; i < m; ++i) {
-        size_t lo, hi;
-        std::cin >> lo >> hi;
-        queries.emplace_back(lo, hi);
+    for (int n = 0; n < N; ++n) {
+        int left;
+        int right;
+        cin >> left;
+        cin >> right;
+        v[n] = make_tuple(left, right, n);
     }
 
-    std::cout << "executing " << m << " range queries" << std::endl;
-    auto root = min_seg_tree.root();
-    start = clock_type::now();
-    for (int run = 0; run < 5; ++run) {
-        for (uint64_t i = 0; i < m; ++i) {
-            auto x = min_seg_tree.rmq(queries[i]);
-            // auto x = min_seg_tree.rmqS(queries[i], root, 0);
-            // std::cout << "rmq in [" << queries[i].lo << ", "
-            //                         << queries[i].hi << "]: " << x << "\n";
-            do_not_optimize_away(x);
-        }
-    }
-    end = clock_type::now();
-    elapsed = end - start;
+    // sort by right in increasing order
+    sort(v.begin(), v.end(),
+        [](const RANGE & lhs, const RANGE & rhs) {
+            return get<R_RIGHT>(lhs) < get<R_RIGHT>(rhs);
+        });
 
-    std::cout << "average query time: "
-              << static_cast<double>(elapsed.count()) / (5 * m) * 1000000
-              << " [musec]" << std::endl;
-
-    uint64_t k = 0; // number of updates
-    std::cin >> k;
-
-    typedef std::pair<segment_tree_type::STRange, int> update_query;
-    std::vector<update_query> updates;
-    updates.reserve(k);
-
-    for (uint64_t i = 0; i < k; ++i) {
-        size_t lo, hi;
-        int delta;
-        std::cin >> lo >> hi >> delta;
-        segment_tree_type::STRange r(lo, hi);
-        updates.emplace_back(r, delta);
+    // tuple<left, sorted_rank_right, rank>
+    for (int n = 0; n < N; ++n) {
+        get<R_RIGHT>(v[n]) = n;
     }
 
-    std::cout << "executing " << k << " updates" << std::endl;
-    start = clock_type::now();
-    for (uint64_t i = 0; i < k; ++i) {
-        auto const& range = updates[i].first;
-        int delta = updates[i].second;
-        min_seg_tree.update(range.lo, delta);
-        // min_seg_tree.updateS(range.lo, delta, root, 0);
+    // sort by left in decreasing order
+    sort(v.begin(), v.end(),
+        [](const RANGE & lhs, const RANGE & rhs) {
+            return get<R_LEFT>(lhs) > get<R_LEFT>(rhs);
+        });
+
+    auto sumOp = [](const int & lhs, const int & rhs){ return lhs + rhs; };
+    SegmentTree<int> st(sumOp, 0);
+    st.buildConstant(N, 0);
+
+    for (int n = 0; n < N; ++n) {
+        const int id = get<R_RIGHT>(v[n]);
+        const int rank = get<R_RANK>(v[n]);
+        results[rank] = st.query(0, id);
+        st.update(id, 1);
     }
-    end = clock_type::now();
-    elapsed = end - start;
 
-    std::cout << "average update time: "
-              << static_cast<double>(elapsed.count()) / k * 1000000
-              << " [musec]" << std::endl;
-
-    std::cout << "executing " << k << " range updates" << std::endl;
-    start = clock_type::now();
-    for (uint64_t i = 0; i < k; ++i) {
-        auto const& range = updates[i].first;
-        int delta = updates[i].second;
-        min_seg_tree.updateRange(range, delta);
-        // min_seg_tree.updateRangeS(range, delta, root, 0);
+    for (int n = 0; n < N; ++n) {
+        cout << results[n] << endl;
     }
-    end = clock_type::now();
-    elapsed = end - start;
+    cout << endl;
 
-    std::cout << "average range update time: "
-              << static_cast<double>(elapsed.count()) / k * 1000000
-              << " [musec]" << std::endl;
-
-    // min_seg_tree.print();
+    results.clear();
+    v.clear();
 
     return 0;
 }
